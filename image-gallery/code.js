a70547ad007eb0200950613ac0df4892b537977a
dellolivo-denis <denis.dellolivo@isisbem.it>
2026-01-12 11:06:05 +0100

    add number guess and gallery

---
+++ image-gallery/code.js
@@ -0,0 +1,52 @@
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+const caption = document.querySelector(".caption");
+const index = document.querySelector(".index");
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+const baseURL ="https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  newImage.tabIndex = "0";
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener("click", updateDisplayedImage);
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
+
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+  updateCaption();
+}
+
+function updateCaption() {
+  caption.textContent = displayedImage.alt;
+}
+
+btn.addEventListener("click", () => {
+  if (btn.classList.contains("dark")) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+  } else {
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+  }
+  btn.classList.toggle("dark");
+});
